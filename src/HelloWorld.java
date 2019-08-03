c623d670fc4cdcfb8796a7fe8229ac539b8b874e
AG AGR <pekran123@hotmail.com>
2019-08-03 22:33:48 +0200

    sdasd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
 	
 	public static void main(String[] args) {
-		System.out.print("hellooEJSHFAKSDHFASLDKHFBo");
+		System.out.print("hellooEJasdasdasdasdHFAKSDHFASLDKHFBo");
 	}
 	
 	
